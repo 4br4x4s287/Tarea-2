14ab6c2b50a3e171824cc990b6532a008f0592bf
4br4x4s287 <114418235+4br4x4s287@users.noreply.github.com>
2024-03-04 21:17:40 -0600

    Add files via upload

---
+++ Ejercicio_POO/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+
+namespace Ejercicio_POO
+{
+    public class Puerta
+    {
+        public string color;
+
+        public Puerta()
+        {
+            color = "Café";
+        }
+
+        public Puerta(string colorInicial)
+        {
+            color = colorInicial;
+        }
+
+        public void SetColor(string nuevoColor)
+        {
+            color = nuevoColor;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Soy una Puerta, mi color es " + color + ".");
+        }
+    }
+
+    public class Casa
+    {
+        public int area;
+        public Puerta puerta;
+
+        public Casa(int areaInicial)
+        {
+            area = areaInicial;
+            puerta = new Puerta();
+        }
+
+        public void SetArea(int nuevaArea)
+        {
+            area = nuevaArea;
+        }
+
+        public void SetPuerta(Puerta nuevaPuerta)
+        {
+            puerta = nuevaPuerta;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Soy una Casa, mi área es " + area + " m2.");
+        }
+    }
+
+    public class Apartamento : Casa
+    {
+        public Apartamento() : base(50) { }
+
+        public new void MostrarDatos()
+        {
+            Console.WriteLine("Soy un Apartamento, mi área es " + area + " m2.");
+        }
+    }
+
+    public class Persona
+    {
+        public string nombre;
+        public Casa casa;
+
+        public Persona()
+        {
+            nombre = "Juan";
+            casa = new Casa(150);
+        }
+
+        public Persona(string nombrePersona, Casa casaPersona)
+        {
+            nombre = nombrePersona;
+            casa = casaPersona;
+        }
+
+        public void SetNombre(string nuevoNombre)
+        {
+            nombre = nuevoNombre;
+        }
+
+        public void SetCasa(Casa nuevaCasa)
+        {
+            casa = nuevaCasa;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine("Mi nombre es " + nombre + ".");
+            casa.MostrarDatos();
+            casa.puerta.MostrarDatos();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.MostrarDatos();
+            persona.SetNombre("Carlos");
+            Apartamento nuevoApartamento = new Apartamento();
+            Puerta nuevaPuerta = new Puerta("Verde");
+            nuevoApartamento.SetPuerta(nuevaPuerta);
+            persona.SetCasa(nuevoApartamento);
+            persona.MostrarDatos();
+
+            Console.ReadLine();
+        }
+    }
+}
